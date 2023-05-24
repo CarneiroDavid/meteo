1d58d7952f0ef617e68272d8cf7923ff3b0d8058
LerautB <bastien.leraut@free.fr>
2023-05-24 16:33:43 +0200

    oublie de sauvegarde du fichier home avant de push

--- src/views/home.js
+++ src/views/home.js
@@ -1,13 +1,10 @@
 import { useState } from 'react';
-import logo from '../VS_logo.svg.png'
-import logo2 from '../VS_logo.svg2.png'
 import SearchForm from '../components/SearchForm/searchForm';
 import Layout from "../layouts/layouts";
 
 function Home() {
 	
     const [data,setData] = useState(true)
-	console.log(typeof data);
     return(
 		<div className='App'>
 			<Layout>
